14a9fb55cd24338b97f15904697ef363dac2a905
tea-eclair <148536484+tea-eclair@users.noreply.github.com>
2023-11-10 14:52:37 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,30 @@
-from flask import Flask, request, render_template, jsonify 
- 
-app = Flask(__name__) 
- 
-# In-memory storage for messages (Replace with a database in a real project) 
-messages = [] 
- 
-@app.route('/send', methods=['POST']) 
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+messages = []
+
+@app.route('/send', methods=['POST'])
 def send_message():
     global messages
-    data = request.get_json() 
-    if 'message' in data: 
-        message = data['message'] 
-        messages.append(message) 
-        return jsonify({"status": "Message sent successfully"}) 
-    else: 
-        return jsonify({"status": "Message format is incorrect"}) 
- 
-@app.route('/get', methods=['GET']) 
+    data = request.get_json()
+    print('Received data:', data)
+    if 'username' in data and 'message' in data:
+        username = data['username']
+        message = data['message']
+        messages.append({'username': username, 'message': message})
+        return jsonify({"status": "Message sent successfully"})
+    else:
+        return jsonify({"status": "Message format is incorrect"})
+
+@app.route('/get', methods=['GET'])
 def get_messages():
-    global messages 
-    return jsonify(messages=messages) 
- 
-@app.route('/', methods=['GET']) 
-def index(): 
-    return render_template('index.html', messages=messages) 
- 
-if __name__ == 'main': 
+    global messages
+    return jsonify(messages=messages)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', messages=messages)
+
+if __name__ == '__main__':
     app.run(debug=True)
